cfd30cd2f8b47ed0e5db88fe0b2d9308eb2ecd91
andrea tramacere <andrea.tramacere@gmail.com>
2017-07-05 12:22:23 +0200

    old changes

--- bin/run_osa_cdci_server.py
+++ bin/run_osa_cdci_server.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+
+
 # -*- encoding: utf-8 -*-
 """
 
@@ -18,20 +21,19 @@ from cdci_data_analysis.flask_app.app import run_app
 from cdci_data_analysis.configurer import ConfigEnv
 
 
-
-
 def main(argv=None):
 	parser = argparse.ArgumentParser()
-	parser.add_argument('-conf_file',type=str,default=None)
+	parser.add_argument('-conf_file', type=str, default=None)
 
 	args = parser.parse_args()
 
-	conf_file=args.conf_file
+	conf_file = args.conf_file
 
-	conf= ConfigEnv.from_conf_file(conf_file)
+	conf = ConfigEnv.from_conf_file(conf_file)
 
 	run_app(conf)
 
-	if __name__ == "__main__":
-		#$port = int(os.environ.get("PORT", 5000))
-		main()
+
+if __name__ == "__main__":
+	# $port = int(os.environ.get("PORT", 5000))
+	main()
--- tests/test_plugins.py
+++ tests/test_plugins.py
@@ -2,7 +2,7 @@
 
 from cdci_data_analysis.configurer import ConfigEnv
 
-osaconf=ConfigEnv("isdc/unsaved_data/savchenk/data/reduced/ddcache","./","localhost",32779,"http://localhost",5000)
+osaconf=ConfigEnv("isdc/unsaved_data/savchenk/data/reduced/ddcache","./","localhost",32778,"http://localhost",5000)
 
     
 
@@ -19,6 +19,8 @@ def test_too_strickt_type_verifications():
         print('--')
     prod.set_par_value('time_group_selector','scw_list')
     prod.show_parameters_list()
+
+
 
 def test_image():
     from cdci_data_analysis.ddosa_interface.osa_image_dispatcher import OSA_ISGRI_IMAGE
@@ -42,6 +44,9 @@ def test_image():
 
     assert sum(out_prod.flatten()>0)>100 # some non-zero pixels
 
+
+
+
 def test_spectrum():
     from cdci_data_analysis.ddosa_interface.osa_spectrum_dispatcher import OSA_ISGRI_SPECTRUM
 
@@ -61,6 +66,8 @@ def test_spectrum():
     print out_prod,exception
 
     print dir(out_prod)
+
+
 
 def test_lightcurve():
     from cdci_data_analysis.ddosa_interface.osa_lightcurve_dispatcher import OSA_ISGRI_LIGHTCURVE
