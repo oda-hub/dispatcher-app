aa92247b0e4bc89effda8b0940c7e57c5743f777
andrea tramacere <orion@MacBook-Pro-3.local>
2019-02-01 16:15:02 +0100

    masking nan in image plot

--- cdci_data_analysis/analysis/plot_tools.py
+++ cdci_data_analysis/analysis/plot_tools.py
@@ -62,7 +62,7 @@ class Image(object):
         w = wcs.WCS(self.header)
         color_mapper = LinearColorMapper(low=min_s, high=max_s, palette=Plasma256)
 
-        fig_im = fig.image(image=[self.data], x=[0], y=[0], dw=[c * 0.5], dh=[r * 0.5],
+        fig_im = fig.image(image=[self.data[msk]], x=[0], y=[0], dw=[c * 0.5], dh=[r * 0.5],
                            color_mapper=color_mapper)
 
         hover = HoverTool(tooltips=[("x", "$x"), ("y", "$y"), ("value", "@image")],
