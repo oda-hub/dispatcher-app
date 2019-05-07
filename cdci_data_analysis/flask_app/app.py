1d25e52f4702b4f066dce1c78e9d274ec28a7b3f
andrea tramacere <orion@MacBook-Pro-3.local>
2019-05-07 15:51:10 +0200

    adding url to fits file

--- cdci_data_analysis/flask_app/app.py
+++ cdci_data_analysis/flask_app/app.py
@@ -808,8 +808,8 @@ class InstrumentQueryBackEnd(object):
 
             print ('*** run_asynch',run_asynch)
             print ('*** api', api)
-            self.instrument.conf=self.conf
-            print('CICCIO, conf',self.conf)
+            self.instrument.config=self.config
+            print('CICCIO, conf',self.config)
             query_out = self.instrument.run_query(product_type,
                                                     self.par_dic,
                                                     request,
