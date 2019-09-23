d2882a55894a69d1f8edd634ba4bffcffbca7324
andrea tramacere <orion@MacBook-Pro-3.local>
2019-09-23 15:39:04 +0200

    adding magic data

--- cdci_data_analysis/flask_app/micro_app.py
+++ cdci_data_analysis/flask_app/micro_app.py
@@ -1,24 +1,19 @@
 from flask import Flask, jsonify, abort
-
+from oda_api.data_products import NumpyDataProduct, NumpyDataUnit
+from astropy.table import Table
+import json
 micro_service = Flask("micro_service")
 
-#in memory data store (in practice this is pulled from a DB)
-class Address():
-    def __init__(self, ID, number, line_1, line_2, post_code):
-        self.ID = ID
-        self.number = number
-        self.line_1 = line_1
-        self.line_2 = line_2
-        self.post_code = post_code
-
-add1 = Address(1, 2, "North Street", "Python City", "PY11 0PY")
-add2 = Address(2, 21, "South Street", "Python City", "PY12 5PY")
-
-addresses = [add1,add2]
 
 @micro_service.route('/address_book/api/v1.0/addresses', methods=['GET'])
-def get_addressess():
-    return jsonify({'addresses': [add.__dict__ for add in addresses ]})
+def get_SED():
+    t = Table.read('MAGIC_data/19e/magic_19e_sed_fig3_mwl_target01.ecsv', format='ascii')
+    t.write('test.fits', format='fits', overwrite=True)
+    p=NumpyDataProduct.from_fits_file('test.fits', meta_data=t.meta, name='SED')
+    out_dict={}
+    out_dict['products']['numpy_data_product_list'] = [p.encode()]
+    # print ( 'ECCO',out_dict['products']['numpy_data_product_list'],_p,_npdl)
+    return jsonify(out_dict)
 
 def run_micro_service(conf,debug=False,threaded=False):
     micro_service.config['conf'] = conf
