03d93faf16484e389b448bb8c84ede82d01ed059
andrea tramacere <orion@MacBook-Pro-3.local>
2019-09-24 12:16:06 +0200

    testing magic data serialization

--- cdci_data_analysis/flask_app/micro_app.py
+++ cdci_data_analysis/flask_app/micro_app.py
@@ -32,6 +32,7 @@ def run_micro_service(conf,debug=False,threaded=False):
     #if conf.sentry_url is not None:
     print('conf micro',micro_service.config['conf'])
     #sentry = Sentry(app, dsn=conf.sentry_url)
+    micro_service.config['JSON_AS_ASCII'] = False
     micro_service.run(host=conf.microservice_url, port=conf.microservice_port, debug=debug,threaded=threaded)
 
 #if __name__ == '__main__':
