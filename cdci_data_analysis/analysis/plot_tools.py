4a82592f463e27d86d7f812d73b82ac7ed34f3f4
andrea tramacere <orion@MacBook-Pro-3.local>
2019-10-11 14:32:07 +0200

    working on js9

--- cdci_data_analysis/analysis/plot_tools.py
+++ cdci_data_analysis/analysis/plot_tools.py
@@ -208,8 +208,6 @@ class Image(object):
                   function init(){                                                                                                                                                             
                      var idx, obj;                                                                                                                                                             
                      %s
-                    JS9.AddRegions("circle" {wcs: "255.205185 -41.654163  FK5"});
-
                      %s                                                                                                                      
                   }                                                                                                                                                                            
                   $(document).ready(function(){                                                                                                                                                
