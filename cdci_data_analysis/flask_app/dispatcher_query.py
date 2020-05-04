0c08e8fd39b45bca01baa5287822272a59733d2b
andrea tramacere <orion@MacBook-Pro-3.local>
2020-05-04 15:37:28 +0200

    check oda api version

--- cdci_data_analysis/flask_app/dispatcher_query.py
+++ cdci_data_analysis/flask_app/dispatcher_query.py
@@ -607,7 +607,31 @@ class InstrumentQueryBackEnd(object):
         print ('==============================> run query <==============================')
 
         if 'api' in self.par_dic.keys():
-            api=True
+            api = True
+            curent_disp_oda_api_version = None
+            if hasattr(oda_api, '__version__'):
+                curent_disp_oda_api_version = oda_api.__version__
+            query_oda_api_version = None
+            if 'oda_api_version' in self.par_dic.keys():
+                query_oda_api_version = self.par_dic['oda_api_version']
+
+            oda_api_version_error = None
+            failed_task = 'oda_api version compatibility'
+            if query_oda_api_version is None or curent_disp_oda_api_version is None:
+                oda_api_version_error = 'oda_api version not compatible, current version=%s, update your oda_api package' % curent_disp_oda_api_version
+
+
+            elif curent_disp_oda_api_version > query_oda_api_version:
+                oda_api_version_error = 'oda_api version not compatible, min=%s, current=%s' % (_min_v, curent_disp_oda_api_version)
+
+            if oda_api_version_error is not None:
+                query_status = 'failed'
+                query_out = QueryOutput()
+                query_out.set_failed(failed_task, message=oda_api_version_error, job_status='failed')
+
+                resp = self.build_dispatcher_response(query_new_status=query_status, query_out=query_out, job_monitor=None, off_line=off_line, api=api)
+                return resp
+
         else:
             api=False
 
