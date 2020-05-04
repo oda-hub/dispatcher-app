67855253eed141f24f4a0d2beb5f14863b357c15
andrea tramacere <orion@MacBook-Pro-3.local>
2020-05-04 15:43:22 +0200

    check oda api version

--- cdci_data_analysis/flask_app/dispatcher_query.py
+++ cdci_data_analysis/flask_app/dispatcher_query.py
@@ -618,11 +618,11 @@ class InstrumentQueryBackEnd(object):
             oda_api_version_error = None
             failed_task = 'oda_api version compatibility'
             if query_oda_api_version is None or current_disp_oda_api_version is None:
-                oda_api_version_error = 'oda_api version not compatible, current version=%s, update your oda_api package' % curent_disp_oda_api_version
+                oda_api_version_error = 'oda_api version not compatible, current version=%s, update your oda_api package' % current_disp_oda_api_version
 
 
             elif current_disp_oda_api_version > query_oda_api_version:
-                oda_api_version_error = 'oda_api version not compatible, min=%s, current=%s' % (_min_v, curent_disp_oda_api_version)
+                oda_api_version_error = 'oda_api version not compatible, min=%s, current=%s' % (_min_v, current_disp_oda_api_version)
 
             if oda_api_version_error is not None:
                 query_status = 'failed'
