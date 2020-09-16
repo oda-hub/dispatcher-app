0b77c2eadaa753178906a55578b4af8c72195c29
andrea tramacere <orion@MacBook-Pro-3.local>
2020-09-16 14:04:13 +0200

    adding extension to js9 request

--- cdci_data_analysis/flask_app/app.py
+++ cdci_data_analysis/flask_app/app.py
@@ -232,7 +232,7 @@ class GetJS9Plot(Resource):
         """
         api_parser = reqparse.RequestParser()
         api_parser.add_argument('file_path', required=True, help="the name of the file",type=str)
-        api_parser.add_argument('ext_id', required=False, help="extension id", type=int)
+        api_parser.add_argument('ext_id', required=False, help="extension id", type=int,default=4)
         api_args = api_parser.parse_args()
         file_path = api_args['file_path']
         ext_id = api_args['ext_id']
@@ -240,7 +240,7 @@ class GetJS9Plot(Resource):
         tmp_file.file_path._set_file_path(tmp_file.file_path.dir_name,'js9.fits')
 
         data=FitsFile(file_path).open()[ext_id]
-        print(data,tmp_file.file_path)
+        #print(data,tmp_file.file_path)
         data.writeto(tmp_file.file_path.path,overwrite=True)
         region_file = None
         if 'region_file' in api_args.keys():
@@ -248,7 +248,7 @@ class GetJS9Plot(Resource):
         print('file_path,region_file', tmp_file.file_path.path, region_file)
         try:
             img = Image(None, None)
-            print('get_js9_plot path',tmp_file.file_path.path)
+            #print('get_js9_plot path',tmp_file.file_path.path)
             img= img.get_js9_html(tmp_file.file_path.path, region_file=region_file)
 
         except Exception as e:
