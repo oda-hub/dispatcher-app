e47f90858391f488a157c1eca5d06bced03e9234
andrea tramacere <orion@MacBook-Pro-3.local>
2020-09-16 16:40:21 +0200

    adding response fro wrong request to run_analysis and added version

--- cdci_data_analysis/flask_app/app.py
+++ cdci_data_analysis/flask_app/app.py
@@ -181,10 +181,10 @@ def run_analysis():
         payload['cdci_data_analysis_version']=__version__
         payload['oda_api_version'] = oda_api.__version__
         payload['error_message'] = str(e)
-        _l = []
+        _l = ''
         for instrument_factory in importer.instrument_facotry_list:
-            _l.append(instrument_factory().name)
-        payload =  str(_l)
+            _l+='%s, '.append(instrument_factory().name)
+        payload['installed_instruments'] = _l
         print(payload)
         raise InvalidUsage('request not valid', status_code=410,payload=payload)
 
