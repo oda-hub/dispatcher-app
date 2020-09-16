3af75090d15a4f36d15226ffb11983b14a5ac4e8
andrea tramacere <orion@MacBook-Pro-3.local>
2020-09-16 12:56:26 +0200

    adding extension to js9 request

--- cdci_data_analysis/flask_app/app.py
+++ cdci_data_analysis/flask_app/app.py
@@ -240,8 +240,8 @@ class GetJS9Plot(Resource):
         tmp_file.file_path._name='js9_tmp.fits'
 
         data=FitsFile(file_path).open()[ext_id]
-        print(data)
-        data.writeto(tmp_file.file_path)
+        print(data,tmp_file.file_path)
+        data.writeto(tmp_file.file_path,overwrite=True,format='fits')
         region_file = None
         if 'region_file' in api_args.keys():
             region_file = api_args['region_file']
