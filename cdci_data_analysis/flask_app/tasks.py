ef2f9f764c1d00784a8a8bc61e0ee80d372a32c6
Volodymyr Savchenko <Volodymyr.Savchenko@unige.ch>
2021-02-22 22:43:53 +0100

    dump output in worker

--- cdci_data_analysis/flask_app/tasks.py
+++ cdci_data_analysis/flask_app/tasks.py
@@ -28,6 +28,8 @@ def request_dispatcher(url, params):
     print("\033[31mquery URL", url, "\033[0m")
     print("\033[31mquery params", params, "\033[0m")
 
-    requests.get(url, params=params)
+    r = requests.get(url, params=params)
+
+    print("\033[35m", r.json(), "\033[0m")
 
     return url + str(params)
