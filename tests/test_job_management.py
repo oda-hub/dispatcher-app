b21e15c2cea379b38e96d8076d5db020d773135b
Volodymyr Savchenko <contact@volodymyrsavchenko.com>
2021-04-01 10:38:35 +0200

    default prefix is empty

--- tests/test_job_management.py
+++ tests/test_job_management.py
@@ -27,12 +27,31 @@ def test_callback_without_prior_run_analysis(dispatcher_live_fixture):
 
     assert c.status_code == 200
 
-#@app.route('/call_back', methods=['POST', 'GET'])
-#def dataserver_call_back():
-    #log = logging.getLogger('werkzeug')
-    #log.disabled = True
-    #app.logger.disabled = True
-#    print('===========================> dataserver_call_back')
-#    query = InstrumentQueryBackEnd(
-#        app, instrument_name='mock', data_server_call_back=True)
-#    query.run_call_back()
+
+def test_callback_after_run_analysis(dispatcher_live_fixture):
+    server = dispatcher_live_fixture
+    print("constructed server:", server)
+
+    c = requests.get(server + "/run_analysis",
+                     params=dict(
+                        query_status="new",
+                        query_type="Real",
+                        instrument="empty",
+                        product_type="numerical",                        
+                        async_dispatcher=False,
+                    ))
+
+    print(c.text)    
+
+    assert c.status_code == 200        
+
+    c = requests.get(server + "/call_back",
+                   params={
+                       'job_id': 'test-job-id',
+                       'instrument_name': 'test-instrument_name',
+                   },
+                )
+
+    print(c.text)    
+
+    assert c.status_code == 200
