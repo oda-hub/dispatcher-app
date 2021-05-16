04ae898d9c0416dd79fbf7daed2fdd1308b3f138
Volodymyr Savchenko <contact@volodymyrsavchenko.com>
2021-05-16 21:29:02 +0200

    logstash change commit

---
+++ cdci_data_analysis/flask_app/logstash.py
@@ -0,0 +1,18 @@
+import logging
+import pylogstash
+
+logger = logging.getLogger(__name__)
+
+def logstash_message(app, message_json: dict):
+    conf = app.config['conf']
+
+    if conf.logstash_host not in [None, "None"] and conf.logstash_port not in [None, "None"]:
+        url = f"{conf.logstash_host}:{conf.logstash_port}"
+
+        pylogstash.LogStasher(url).log(
+            message_json
+        )
+
+    logger.debug(f"\033[35m stashing to {conf.logstash_host}:{conf.logstash_port}\033[0m")
+    logger.debug(f"\033[35m{message_json}\033[0m")
+    
