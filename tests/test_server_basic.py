411515e75a2f1d59872d3b11ae494f16accca53c
burnout87 <gabriele.barni@gmail.com>
2021-07-01 15:33:45 +0200

    message and adapted test

--- tests/test_server_basic.py
+++ tests/test_server_basic.py
@@ -449,7 +449,7 @@ def test_invalid_token(dispatcher_live_fixture):
                 expected_status_code=403
                 )
 
-    assert jdata['error_message'] == 'token expired'
+    assert jdata['error_message'] == 'the token provided is expired, please try to logout and login again'
     logger.info("Json output content")
     logger.info(json.dumps(jdata, indent=4))
 
