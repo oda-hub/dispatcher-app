421319cf0ad293a61f82cbf86a7494fe2e2140f7
burnout87 <gabriele.barni@gmail.com>
2021-11-18 09:55:39 +0100

    what breaks?

--- cdci_data_analysis/analysis/drupal_helper.py
+++ cdci_data_analysis/analysis/drupal_helper.py
@@ -1,49 +1,49 @@
-import os
-import json
+# import os
+# import json
 # import requests
 
-from cdci_data_analysis.analysis import email_helper
-from ..analysis.exceptions import RequestNotUnderstood
-from ..flask_app.templates import body_article_product_gallery
+# from cdci_data_analysis.analysis import email_helper
+# from ..analysis.exceptions import RequestNotUnderstood
+# from ..flask_app.templates import body_article_product_gallery
 
 
 def post_to_product_gallery(session_id, job_id):
-    body_gallery_node = body_article_product_gallery.body.copy()
-    # get products
-    scratch_dir_json_fn = f'scratch_sid_{session_id}_jid_{job_id}'
-    # the aliased version might have been created
-    scratch_dir_json_fn_aliased = f'scratch_sid_{session_id}_jid_{job_id}_aliased'
-    analysis_parameters_json_content_original = None
-    query_output_json_content_original = None
-    body_value = None
+    # body_gallery_node = body_article_product_gallery.body.copy()
+    # # get products
+    # scratch_dir_json_fn = f'scratch_sid_{session_id}_jid_{job_id}'
+    # # the aliased version might have been created
+    # scratch_dir_json_fn_aliased = f'scratch_sid_{session_id}_jid_{job_id}_aliased'
+    # analysis_parameters_json_content_original = None
+    # query_output_json_content_original = None
+    # body_value = None
     output_post = None
+    # #
+    # if os.path.exists(scratch_dir_json_fn):
+    #     analysis_parameters_json_content_original = json.load(open(scratch_dir_json_fn + '/analysis_parameters.json'))
+    #     query_output_json_content_original = json.load(open(scratch_dir_json_fn + '/query_output.json'))
+    # elif os.path.exists(scratch_dir_json_fn_aliased):
+    #     analysis_parameters_json_content_original = json.load(
+    #         open(scratch_dir_json_fn_aliased + '/analysis_parameters.json'))
+    #     query_output_json_content_original = json.load(open(scratch_dir_json_fn_aliased + '/query_output.json'))
     #
-    if os.path.exists(scratch_dir_json_fn):
-        analysis_parameters_json_content_original = json.load(open(scratch_dir_json_fn + '/analysis_parameters.json'))
-        query_output_json_content_original = json.load(open(scratch_dir_json_fn + '/query_output.json'))
-    elif os.path.exists(scratch_dir_json_fn_aliased):
-        analysis_parameters_json_content_original = json.load(
-            open(scratch_dir_json_fn_aliased + '/analysis_parameters.json'))
-        query_output_json_content_original = json.load(open(scratch_dir_json_fn_aliased + '/query_output.json'))
+    # if analysis_parameters_json_content_original is not None:
+    #     analysis_parameters_json_content_original.pop('token', None)
+    #     analysis_parameters_json_content_original_str = email_helper.wrap_python_code(
+    #         json.dumps(analysis_parameters_json_content_original))
+    #     body_value = 'Body of the article with the analysis_parameters.json: <br/><br/>' \
+    #                  '<div style="background-color: lightgray; display: inline-block; padding: 5px;">' + \
+    #                  analysis_parameters_json_content_original_str.replace("\n", "<br>") + '</div>'
+    #     instrument = analysis_parameters_json_content_original['instrument']
+    #     product_type = analysis_parameters_json_content_original['product_type']
+    # else:
+    #     raise RequestNotUnderstood(message="Request data ont found",
+    #                                payload={'error_message': 'error while posting article'})
 
-    if analysis_parameters_json_content_original is not None:
-        analysis_parameters_json_content_original.pop('token', None)
-        analysis_parameters_json_content_original_str = email_helper.wrap_python_code(
-            json.dumps(analysis_parameters_json_content_original))
-        body_value = 'Body of the article with the analysis_parameters.json: <br/><br/>' \
-                     '<div style="background-color: lightgray; display: inline-block; padding: 5px;">' + \
-                     analysis_parameters_json_content_original_str.replace("\n", "<br>") + '</div>'
-        instrument = analysis_parameters_json_content_original['instrument']
-        product_type = analysis_parameters_json_content_original['product_type']
-    else:
-        raise RequestNotUnderstood(message="Request data ont found",
-                                   payload={'error_message': 'error while posting article'})
-
-    body_gallery_node["body"][0]["value"] = body_value
+    # body_gallery_node["body"][0]["value"] = body_value
 
     headers = {
         'Content-type': 'application/hal+json',
-        'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2MzcyMjAxNDcsImV4cCI6MTYzNzIyMzc0NywiZHJ1cGFsIjp7InVpZCI6IjQifX0.UhCxoRjRiyVX2ILhZRXAineNZ3cmxc-b-fHra00chFE'
+        'Authorization': 'Bearer '
     }
     # # get taxonomy info for the instrument
     # log_res = requests.get("http://cdciweb02.isdc.unige.ch/mmoda-pg/taxonomy/term_name/" + instrument,
