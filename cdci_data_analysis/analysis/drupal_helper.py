3e26817e8cd4ce1c96c778cb03905c5318f6e7b7
burnout87 <gabriele.barni@gmail.com>
2021-11-18 09:52:08 +0100

    what breaks?

--- cdci_data_analysis/analysis/drupal_helper.py
+++ cdci_data_analysis/analysis/drupal_helper.py
@@ -1,6 +1,6 @@
 import os
 import json
-import requests
+# import requests
 
 from cdci_data_analysis.analysis import email_helper
 from ..analysis.exceptions import RequestNotUnderstood
@@ -16,6 +16,7 @@ def post_to_product_gallery(session_id, job_id):
     analysis_parameters_json_content_original = None
     query_output_json_content_original = None
     body_value = None
+    output_post = None
     #
     if os.path.exists(scratch_dir_json_fn):
         analysis_parameters_json_content_original = json.load(open(scratch_dir_json_fn + '/analysis_parameters.json'))
@@ -44,33 +45,33 @@ def post_to_product_gallery(session_id, job_id):
         'Content-type': 'application/hal+json',
         'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2MzcyMjAxNDcsImV4cCI6MTYzNzIyMzc0NywiZHJ1cGFsIjp7InVpZCI6IjQifX0.UhCxoRjRiyVX2ILhZRXAineNZ3cmxc-b-fHra00chFE'
     }
-    # get taxonomy info for the instrument
-    log_res = requests.get("http://cdciweb02.isdc.unige.ch/mmoda-pg/taxonomy/term_name/" + instrument,
-                           headers=headers
-                           )
-    output_post = log_res.json()
-    if len(output_post) > 0:
-        body_gallery_node['field_instrument'] = [{
-            "target_id": output_post[0]['tid']
-        }]
-    # get taxonomy info for the product
-    log_res = requests.get("http://cdciweb02.isdc.unige.ch/mmoda-pg/taxonomy/term_name/" + product_type,
-                           headers=headers
-                           )
-    output_post = log_res.json()
-    if len(output_post) > 0:
-        body_gallery_node['field_product'] = [{
-            "target_id": output_post[0]['tid']
-        }]
-    # post an article
-    log_res = requests.post("http://cdciweb02.isdc.unige.ch/mmoda-pg/node?_format=hal_json",
-                            data=json.dumps(body_gallery_node),
-                            headers=headers
-                            )
-    output_post = log_res.json()
-    if log_res.status_code < 200 or log_res.status_code >= 300:
-        raise RequestNotUnderstood(output_post['message'],
-                                   status_code=log_res.status_code,
-                                   payload={'error_message': 'error while posting article'})
+    # # get taxonomy info for the instrument
+    # log_res = requests.get("http://cdciweb02.isdc.unige.ch/mmoda-pg/taxonomy/term_name/" + instrument,
+    #                        headers=headers
+    #                        )
+    # output_post = log_res.json()
+    # if len(output_post) > 0:
+    #     body_gallery_node['field_instrument'] = [{
+    #         "target_id": output_post[0]['tid']
+    #     }]
+    # # get taxonomy info for the product
+    # log_res = requests.get("http://cdciweb02.isdc.unige.ch/mmoda-pg/taxonomy/term_name/" + product_type,
+    #                        headers=headers
+    #                        )
+    # output_post = log_res.json()
+    # if len(output_post) > 0:
+    #     body_gallery_node['field_product'] = [{
+    #         "target_id": output_post[0]['tid']
+    #     }]
+    # # post an article
+    # log_res = requests.post("http://cdciweb02.isdc.unige.ch/mmoda-pg/node?_format=hal_json",
+    #                         data=json.dumps(body_gallery_node),
+    #                         headers=headers
+    #                         )
+    # output_post = log_res.json()
+    # if log_res.status_code < 200 or log_res.status_code >= 300:
+    #     raise RequestNotUnderstood(output_post['message'],
+    #                                status_code=log_res.status_code,
+    #                                payload={'error_message': 'error while posting article'})
 
     return output_post
