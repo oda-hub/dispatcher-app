673286f863d33210d41d6a2ae739e8302eb631d3
burnout87 <gabriele.barni@gmail.com>
2021-12-01 10:05:55 +0100

    reverting and TODOs

--- cdci_data_analysis/analysis/queries.py
+++ cdci_data_analysis/analysis/queries.py
@@ -525,15 +525,26 @@ class ProductQuery(BaseQuery):
 
         except Exception as e:
             # TODO: could we avoid these? they make error tracking hard
+            # TODO we could use the very same approach used when test_communication fails
             logger.exception("failed to get query products")
             e_message = repr(e) + '\n' + getattr(e, 'message', '') + '\n' + getattr(e, 'debug_message', '')
 
             #status=1
             job.set_failed()
-            # if os.environ.get('DISPATCHER_DEBUG_MODE', 'yes') == 'yes':
-            #     raise
+            if os.environ.get('DISPATCHER_DEBUG', 'yes') == 'yes':
+                raise
 
-            raise InternalError(e_message)
+            e_message = getattr(e, 'message', '')
+            debug_message = repr(e) + ' : ' + getattr(e, 'debug_message', '')
+
+            query_out.set_failed('get_dataserver_products found job failed',
+                                 logger=logger,
+                                 sentry_client=sentry_client,
+                                 excep=e,
+                                 e_message=e_message,
+                                 debug_message=debug_message)
+            # TODO to use this approach when we will refactor the handling of exceptions
+            # raise InternalError(e_message)
 
         logger.info('--> data_server_query_status %d' % query_out.get_status())
         logger.info('--> end product query ')
