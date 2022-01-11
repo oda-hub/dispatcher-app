729d5bbd1234c64f57593620ca360c389fb8435a
burnout87 <gabriele.barni@gmail.com>
2022-01-11 11:08:01 +0100

    better 404 error message

--- cdci_data_analysis/analysis/drupal_helper.py
+++ cdci_data_analysis/analysis/drupal_helper.py
@@ -92,8 +92,9 @@ def execute_drupal_request(url,
                 raise RequestNotAuthorized(error_msg)
 
             if res.status_code == 404:
-                logger.warning(f"there seems to be some problem in completing a request to the product gallery,"
-                               " this might be due to an error in the url or the page requested no longer exists, "
+                logger.warning(f"there seems to be some problem in completing a request to the product gallery:\n"
+                               f"the requested url {url} lead to the error {res.text}, "
+                               "this might be due to an error in the url or the page requested no longer exists, "
                                "please check it and try to issue again the request")
                 raise InternalError('issue when performing a request to the product gallery',
                                     status_code=500,
