9a01b7ad013cf68b584c109c63ef59e1a0488e67
burnout87 <gabriele.barni@gmail.com>
2022-02-11 11:34:37 +0100

    generate renku valid url to start project on the new branch

--- cdci_data_analysis/analysis/renku_helper.py
+++ cdci_data_analysis/analysis/renku_helper.py
@@ -19,11 +19,13 @@ def push_api_code(api_code,
                   renku_gitlab_token,
                   sentry_client=None):
     error_message = 'Error while {step}'
+    repository_folder_path = None
     try:
         step = 'cloning repository'
         repo = clone_renku_repo(renku_repository_url,
                                 renku_gitlab_token_name=renku_gitlab_token_name,
                                 renku_gitlab_token=renku_gitlab_token)
+        repository_folder_path = repo.working_dir
         step = 'assigning branch name'
         branch_name = get_branch_name(job_id=job_id)
 
@@ -36,6 +38,9 @@ def push_api_code(api_code,
         step = f'committing and pushing notebook {new_file_path} to the repository'
         commit_and_push_file(repo, new_file_path)
 
+        step = f'generating a valid url to start a new session on the new branch'
+        renku_session_url = generate_renku_session_url(repo)
+
     except Exception as e:
         error_message = error_message.format(step=step)
 
@@ -45,9 +50,30 @@ def push_api_code(api_code,
         raise RequestNotUnderstood(error_message)
     finally:
         logger.info("==> removing repository folder, since it is no longer necessary")
-        remove_repository(repo)
+        remove_repository(repository_folder_path)
     # TODO to actually return the renkulab url of the newly created branch
-    return repo.remotes.origin.url
+    return renku_session_url
+
+
+def generate_renku_session_url(repo):
+    generated_renku_new_session_url = None
+    original_url = repo.remotes.origin.url
+
+    original_url_parsed = urlparse(original_url)
+
+    # in our case the namespace and project_name are to be provided, extracted from the url of the repository
+    new_session_autostart_url = "{scheme}://{hostname}/projects/{namespace}/{project_name}/sessions/new?autostart=1{branch}"
+
+    # extract namespace
+    namespace = original_url_parsed.path.split('/')[2]
+    # get name of the repository/project
+    project_name = get_repo_name(original_url)
+
+    generated_renku_new_session_url = new_session_autostart_url.format(
+        scheme=original_url_parsed.scheme, hostname=original_url_parsed.hostname,
+        namespace=namespace, project_name=project_name, branch=f'&branch={repo.active_branch}')
+
+    return generated_renku_new_session_url
 
 
 def get_repo_name(repository_url):
@@ -127,7 +153,7 @@ def commit_and_push_file(repo, file_path):
         raise e
 
 
-def remove_repository(repo):
-    if os.path.exists(repo.working_dir):
-        shutil.rmtree(repo.working_dir)
+def remove_repository(repo_working_dir_path):
+    if repo_working_dir_path is not None and os.path.exists(repo_working_dir_path):
+        shutil.rmtree(repo_working_dir_path)
 
