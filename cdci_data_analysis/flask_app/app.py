568be93fe60fbf7512cf7136bec923686af5d7fa
burnout87 <gabriele.barni@gmail.com>
2022-04-25 11:11:25 +0200

    highlight missing role renku error message

--- cdci_data_analysis/flask_app/app.py
+++ cdci_data_analysis/flask_app/app.py
@@ -247,10 +247,10 @@ def push_renku_branch():
     # TODO could not think of better name
     required_roles = ['renku contributor']
     if not all(item in roles for item in required_roles):
-        lacking_roles = ", ".join(sorted(list(set(required_roles) - set(roles))))
+        lacking_roles = "\n".join(['- <b>' + r + '</b>' for r in sorted(list(set(required_roles) - set(roles)))])
         message = (
             f"Unfortunately, your privileges are not sufficient to perform this operation, "
-            f"the following roles are missing: {lacking_roles}.\n"
+            f"the following roles are missing:\n\n{lacking_roles}\n\n"
             f"If you are interested in exploring this functionality you can contact us by clicking the button \"Contact us\" "
             f"at the top of the page and request to have this role assigned."
         )
