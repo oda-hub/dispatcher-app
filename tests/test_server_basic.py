807f0c2605336a2d307a09ed072a54a52203cc06
burnout87 <gabriele.barni@gmail.com>
2023-01-12 11:42:56 +0100

    dedicated test

--- tests/test_server_basic.py
+++ tests/test_server_basic.py
@@ -400,6 +400,77 @@ def test_download_products_public(dispatcher_long_living_fixture, empty_products
     assert data_downloaded == empty_products_files_fixture['content']
 
 
+def test_query_restricted_instrument(dispatcher_live_fixture):
+    server = dispatcher_live_fixture
+
+
+    logger.info("constructed server: %s", server)
+
+    params = {
+        **default_params,
+        'product_type': 'dummy',
+        'query_type': "Dummy",
+        'instrument': 'empty-development',
+    }
+
+    jdata = ask(server,
+                params,
+                expected_status_code=403,
+                expected_query_status=None,
+                max_time_s=150
+                )
+
+    assert jdata["debug_message"] == ""
+    assert jdata["error_message"] == "Unfortunately, your priviledges are not sufficient to make the request for this instrument.\n"
+
+    # let's generate a valid token with high threshold
+    token_payload = {
+        **default_token_payload,
+        "roles": "oda workflow developer"
+    }
+
+    encoded_token = jwt.encode(token_payload, secret_key, algorithm='HS256')
+
+    params = {
+        **default_params,
+        'product_type': 'dummy',
+        'query_type': "Dummy",
+        'instrument': 'empty-development',
+        'token': encoded_token
+    }
+
+    jdata = ask(server,
+                params,
+                expected_query_status=["done"],
+                max_time_s=50
+                )
+
+    token_payload = {
+        **default_token_payload,
+        "roles": "general"
+    }
+
+    encoded_token = jwt.encode(token_payload, secret_key, algorithm='HS256')
+
+    params = {
+        **default_params,
+        'product_type': 'dummy',
+        'query_type': "Dummy",
+        'instrument': 'empty-development',
+        'token': encoded_token
+    }
+
+    jdata = ask(server,
+                params,
+                expected_status_code=403,
+                expected_query_status=None,
+                max_time_s=150
+                )
+
+    assert jdata["debug_message"] == ""
+    assert jdata["error_message"] == "Unfortunately, your priviledges are not sufficient to make the request for this instrument.\n"
+
+
 @pytest.mark.fast
 @pytest.mark.parametrize("endpoint_url", ["instr-list", "api/instr-list"])
 def test_per_user_instrument_list(dispatcher_live_fixture, endpoint_url):
