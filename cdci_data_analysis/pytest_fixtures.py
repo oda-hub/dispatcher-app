bcbb6c4f39fc42229f76953799c79c64d4515a98
burnout87 <gabriele.barni@gmail.com>
2023-01-25 23:17:09 +0100

    dedicated dispatcher fixture

--- cdci_data_analysis/pytest_fixtures.py
+++ cdci_data_analysis/pytest_fixtures.py
@@ -409,6 +409,17 @@ def dispatcher_test_conf_empty_sentry_fn(dispatcher_test_conf_fn):
 
     yield fn
 
+@pytest.fixture
+def dispatcher_test_conf_with_local_products_url_fn(dispatcher_test_conf_fn):
+    fn = dispatcher_test_conf_fn
+    with open(fn, "r+") as f:
+        data = f.read()
+        data = re.sub('(\s+products_url:).*\n', '\n    products_url: http://0.0.0.0:8011\n', data)
+        f.seek(0)
+        f.write(data)
+        f.truncate()
+
+    yield fn
 
 @pytest.fixture
 def dispatcher_test_conf_with_gallery_fn(dispatcher_test_conf_fn):
@@ -767,6 +778,19 @@ def dispatcher_live_fixture_with_gallery(pytestconfig, dispatcher_test_conf_with
 @pytest.fixture
 def dispatcher_live_fixture_with_gallery_no_resolver(pytestconfig, dispatcher_test_conf_with_gallery_no_resolver_fn, dispatcher_debug):
     dispatcher_state = start_dispatcher(pytestconfig.rootdir, dispatcher_test_conf_with_gallery_no_resolver_fn)
+
+    service = dispatcher_state['url']
+    pid = dispatcher_state['pid']
+
+    yield service
+
+    kill_child_processes(pid, signal.SIGINT)
+    os.kill(pid, signal.SIGINT)
+
+
+@pytest.fixture
+def dispatcher_live_fixture_with_local_products_url(pytestconfig, dispatcher_test_conf_with_local_products_url_fn, dispatcher_debug):
+    dispatcher_state = start_dispatcher(pytestconfig.rootdir, dispatcher_test_conf_with_local_products_url_fn)
 
     service = dispatcher_state['url']
     pid = dispatcher_state['pid']
