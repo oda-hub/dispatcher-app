faa1d3345e627b93cdf23f4f066929b952b90283
burnout87 <gabriele.barni@gmail.com>
2023-01-26 11:15:50 +0100

    includes dispatch-data within the url

--- cdci_data_analysis/flask_app/app.py
+++ cdci_data_analysis/flask_app/app.py
@@ -93,7 +93,7 @@ def run_api_instr_list():
     logger.warning('\nThe endpoint /api/instr-list is deprecated and you will be automatically redirected to the '
                    '\'instr-list\' endpoint. Please use this one in the future.\n')
 
-    redirection_url = os.path.join(app.config['conf'].products_url, 'dispatch-data', 'instr-list')
+    redirection_url = os.path.join(app.config['conf'].products_url, 'dispatch-data/instr-list')
 
     if request.args:
         redirection_url += f'?{urlencode(request.args)}'
