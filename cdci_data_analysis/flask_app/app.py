dc6661c2c0ec27cdd0e773ce9cf64ea3e6cd730d
burnout87 <gabriele.barni@gmail.com>
2023-05-03 17:28:30 +0200

    no token check

--- cdci_data_analysis/flask_app/app.py
+++ cdci_data_analysis/flask_app/app.py
@@ -766,38 +766,38 @@ def get_astro_entity_info_by_source_name():
     logger.info("request.args: %s ", request.args)
     logger.info("request.files: %s ", request.files)
 
-    token = request.args.get('token', None)
     app_config = app.config.get('conf')
-    secret_key = app_config.secret_key
-
-    output, output_code = tokenHelper.validate_token_from_request(token=token, secret_key=secret_key,
-                                                                  action="getting the info of an astro entity from the product gallery")
-
-    if output_code is not None:
-        if output is None or not isinstance(output, str):
-            output = 'Error while validating the token'
-        return make_response(output, output_code)
-
-    decoded_token = output
-
-    par_dic = request.values.to_dict()
-    par_dic.pop('token')
+    # token = request.args.get('token', None)
+    # secret_key = app_config.secret_key
+    #
+    # output, output_code = tokenHelper.validate_token_from_request(token=token, secret_key=secret_key,
+    #                                                               action="getting the info of an astro entity from the product gallery")
+    #
+    # if output_code is not None:
+    #     if output is None or not isinstance(output, str):
+    #         output = 'Error while validating the token'
+    #     return make_response(output, output_code)
+    #
+    # decoded_token = output
+    #
+    # par_dic = request.values.to_dict()
+    # par_dic.pop('token')
 
     sentry_dsn = sentry.sentry_url
 
     gallery_secret_key = app_config.product_gallery_secret_key
     product_gallery_url = app_config.product_gallery_url
-    user_email = tokenHelper.get_token_user_email_address(decoded_token)
-    user_id_product_creator = drupal_helper.get_user_id(product_gallery_url=product_gallery_url,
-                                                        user_email=user_email,
-                                                        sentry_dsn=sentry_dsn)
+    # user_email = tokenHelper.get_token_user_email_address(decoded_token)
+    # user_id_product_creator = drupal_helper.get_user_id(product_gallery_url=product_gallery_url,
+    #                                                     user_email=user_email,
+    #                                                     sentry_dsn=sentry_dsn)
     # update the token
-    gallery_jwt_token = drupal_helper.generate_gallery_jwt_token(gallery_secret_key, user_id=user_id_product_creator)
+    # gallery_jwt_token = drupal_helper.generate_gallery_jwt_token(gallery_secret_key, user_id=user_id_product_creator)
 
     src_name = request.args.get('src_name', None)
 
     source_entity_info = drupal_helper.get_source_astrophysical_entity_info_by_source_and_alternative_name(product_gallery_url,
-                                                                                                          gallery_jwt_token,
+                                                                                                          gallery_jwt_token=None,
                                                                                                           source_name=src_name,
                                                                                                           sentry_dsn=sentry_dsn)
 
