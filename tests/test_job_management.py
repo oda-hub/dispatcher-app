1ee0132b14d78c2d8184e74195c1ee5320f72451
burnout87 <gabriele.barni@gmail.com>
2023-11-06 19:32:41 +0100

    Adapted test

--- tests/test_job_management.py
+++ tests/test_job_management.py
@@ -1437,7 +1437,7 @@ def test_status_details_email_done(gunicorn_dispatcher_live_fixture, dispatcher_
     # check the additional status details within the email
     assert 'email_status_details' in jdata
     assert jdata['email_status_details'] == {
-        'exception_message': 'failing query\nInstrument: empty, product: failing failed!\n',
+        'exception_message': 'Error when getting query products\nInstrument: empty, product: failing\n',
         'status': 'empty_product'
     }
 
