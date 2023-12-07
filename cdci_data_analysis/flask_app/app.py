67b3c8154a48fb0539a316c293dbeac7559dd97e
burnout87 <gabriele.barni@gmail.com>
2023-12-07 11:24:00 +0100

    token check outside of inspect_State function

--- cdci_data_analysis/flask_app/app.py
+++ cdci_data_analysis/flask_app/app.py
@@ -277,14 +277,34 @@ def free_up_space():
 
 @app.route('/inspect-state', methods=['POST', 'GET'])
 def inspect_state():
+    token = request.args.get('token', None)
+
+    app_config = app.config.get('conf')
+    secret_key = app_config.secret_key
+    output, output_code = tokenHelper.validate_token_from_request(token=token, secret_key=secret_key,
+                                                                  required_roles=['job manager'],
+                                                                  action="inspect the state for a given job_id")
+
+    if output_code is not None:
+        return make_response(output, output_code)
     state_data_obj = InstrumentQueryBackEnd.inspect_state(app)
-    return jsonify(state_data_obj['records'])
+    return jsonify(dict(records=state_data_obj['records']))
 
 
 @app.route('/inspect-jobs', methods=['POST', 'GET'])
 def inspect_jobs():
+    token = request.args.get('token', None)
+
+    app_config = app.config.get('conf')
+    secret_key = app_config.secret_key
+    output, output_code = tokenHelper.validate_token_from_request(token=token, secret_key=secret_key,
+                                                                  required_roles=['job manager'],
+                                                                  action="inspect the state for a given job_id")
+
+    if output_code is not None:
+        return make_response(output, output_code)
     state_data_obj = InstrumentQueryBackEnd.inspect_state(app)
-    return jsonify(state_data_obj['jobs'])
+    return jsonify(dict(jobs=state_data_obj['jobs']))
 
 @app.route('/instr-list')
 def instr_list():
