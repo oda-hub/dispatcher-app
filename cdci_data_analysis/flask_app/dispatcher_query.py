ead2545decaace7ccfcbbbdbca95184dd7ed437c
Denys SAVCHENKO <speleoden@gmail.com>
2024-01-26 17:53:06 +0100

    json-decode only structured; keep catalog encoded

--- cdci_data_analysis/flask_app/dispatcher_query.py
+++ cdci_data_analysis/flask_app/dispatcher_query.py
@@ -828,12 +828,19 @@ class InstrumentQueryBackEnd:
         else:
             raise NotImplementedError
 
-        self.par_dic = args.to_dict()
+        #self.par_dic = args.to_dict()
 
         self.par_dic = {}
         for k, v in args.to_dict().items():
+            if k in ['catalog_selected_objects', 'selected_catalog']:
+                self.par_dic[k] = v
+                continue
             try:
-                self.par_dic[k] = json.loads(v)
+                decoded = json.loads(v)
+                if isinstance(decoded, (dict, list)):
+                    self.par_dic[k] = decoded
+                else:
+                    self.par_dic[k] = v
             except json.JSONDecodeError:
                 self.par_dic[k] = v
         
