66595471fa969783d473dd02a3d2152a060792a0
Denys SAVCHENKO <speleoden@gmail.com>
2024-03-15 21:50:49 +0100

    remove duplication

--- cdci_data_analysis/flask_app/dispatcher_query.py
+++ cdci_data_analysis/flask_app/dispatcher_query.py
@@ -1069,9 +1069,6 @@ class InstrumentQueryBackEnd:
 
             self.validate_job_id(request_parameters_from_scratch_dir=True)
             file_list = self.args.get('file_list').split(',')
-            for fn in file_list:
-                if "../" in fn:
-                    raise RequestNotAuthorized('No such file')
                         
             file_name = self.args.get('download_file_name')
 
