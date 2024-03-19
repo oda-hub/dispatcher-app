dcd7d4438ecbc108878d245b8ed79664ad178c27
burnout87 <gabriele.barni@gmail.com>
2024-03-19 11:47:59 +0100

    for test

--- cdci_data_analysis/plugins/dummy_plugin/empty_instrument.py
+++ cdci_data_analysis/plugins/dummy_plugin/empty_instrument.py
@@ -82,7 +82,7 @@ def my_instr_factory():
     numerical_query = DataServerNumericQuery('numerical_parameters_dummy_query',
                                              parameters_list=[p])
 
-    f = FileReference(name='dummy_file')
+    f = FileReference(downloadable=True, name='dummy_file')
     file_query = FileParameterQuery('file_parameters_dummy_query',
                                     parameters_list=[p, f])
 
