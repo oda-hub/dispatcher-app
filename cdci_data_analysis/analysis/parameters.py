3d9c8b755e79143583036d287b9975cab332604c
burnout87 <gabriele.barni@gmail.com>
2024-05-22 00:10:05 +0200

    POSIXPath class

--- cdci_data_analysis/analysis/parameters.py
+++ cdci_data_analysis/analysis/parameters.py
@@ -589,6 +589,9 @@ class Name(String):
 class FileReference(String):
     owl_uris = String.owl_uris + ("http://odahub.io/ontology#FileReference",)
 
+class POSIXPath(FileReference):
+    owl_uris = FileReference.owl_uris + ("http://odahub.io/ontology#POSIXPath",)
+
 class FileURL(FileReference):
     owl_uris = FileReference.owl_uris + ("http://odahub.io/ontology#FileURL",)
 
