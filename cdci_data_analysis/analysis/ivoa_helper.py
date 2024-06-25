a4c616c247c405e8d76e19c1308b15dfbcd56b9f
burnout87 <gabriele.barni@gmail.com>
2024-06-25 10:04:22 +0200

    more error check

--- cdci_data_analysis/analysis/ivoa_helper.py
+++ cdci_data_analysis/analysis/ivoa_helper.py
@@ -1,5 +1,6 @@
 from queryparser.adql import ADQLQueryTranslator
 from queryparser.postgresql import PostgreSQLQueryProcessor
+from queryparser.exceptions import QuerySyntaxError
 
 from ..app_logging import app_logging
 
@@ -8,14 +9,22 @@ logger = app_logging.getLogger('ivoa_helper')
 
 
 def parse_adql_query(query):
-    adt = ADQLQueryTranslator(query)
-    qp = PostgreSQLQueryProcessor()
-    qp.set_query(adt.to_postgresql())
-    qp.process_query()
+    try:
+        adt = ADQLQueryTranslator(query)
+        qp = PostgreSQLQueryProcessor()
+        qp.set_query(adt.to_postgresql())
+        qp.process_query()
 
-    output_obj = dict(
-        columns = qp.columns,
-        display_columns = qp.display_columns,
-    )
-
+        output_obj = dict(
+            columns = qp.columns,
+            display_columns = qp.display_columns,
+            tables = qp.tables,
+        )
+    except QuerySyntaxError as qe:
+        logger.error(f'Error parsing ADQL query: {qe}')
+        output_obj = dict(
+            columns = [],
+            display_columns = [],
+            tables = [],
+        )
     return output_obj
