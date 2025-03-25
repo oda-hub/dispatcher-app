5865cc318520a237e0dd7d43ed9df27c64eeb734
burnout87 <gabriele.barni@gmail.com>
2025-03-25 16:09:39 +0100

    init db, more tests

---
+++ tests/gallery_pg_db_data/pg_gallery_db_init_data_products.sql
@@ -0,0 +1,14 @@
+INSERT INTO mmoda_pg_dev.data_product_table_view_v (title, product_path, e1_kev, e2_kev, ra, "dec", time_bin, instrument_name, product_type_name, rev1, rev2, t_start, t_stop, proposal_id, sources, file_name, file_uri, image_uri)
+    VALUES ('test1', 'http://cdciweb02.astro.unige.ch/mmoda/gallery/data-products/emptydummy2022-01-11-121021', 1.0, 2.0, 95.23, 145, 5.0, 'test', 'test', 6, 7, '2020-01-01T00:00:00', '2020-01-01T00:00:00', 'test', 'test', 'test', 'test', 'test');
+
+INSERT INTO mmoda_pg_dev.data_product_table_view_v (title, product_path, e1_kev, e2_kev, ra, "dec", time_bin, instrument_name, product_type_name, rev1, rev2, t_start, t_stop, proposal_id, sources, file_name, file_uri, image_uri)
+    VALUES ('test2', 'http://cdciweb02.astro.unige.ch/mmoda/gallery/data-products/emptydummy2022-01-11-121028', 1.0, 2.0, 55, 46, 5.0, 'test', 'test', 6, 7, '2020-01-01T00:00:00', '2020-01-01T00:00:00', 'test', 'test', 'test', 'test', 'test');
+
+INSERT INTO mmoda_pg_dev.data_product_table_view_v (title, product_path, e1_kev, e2_kev, ra, "dec", time_bin, instrument_name, product_type_name, rev1, rev2, t_start, t_stop, proposal_id, sources, file_name, file_uri, image_uri)
+    VALUES ('test3', 'http://cdciweb02.astro.unige.ch/mmoda/gallery/data-products/emptydummy2022-01-11-121026', 1.0, 2.0, 266.417, -29.0078, 5.0, 'test', 'test', 6, 7, '2020-01-01T00:00:00', '2020-01-01T00:00:00', 'test', 'test', 'test', 'test', 'test');
+
+INSERT INTO mmoda_pg_dev.data_product_table_view_v (title, product_path, e1_kev, e2_kev, ra, "dec", time_bin, instrument_name, product_type_name, rev1, rev2, t_start, t_stop, proposal_id, sources, file_name, file_uri, image_uri)
+    VALUES ('test4', 'http://cdciweb02.astro.unige.ch/mmoda/gallery/data-products/emptydummy2022-01-11-121030', 1.0, 2.0, 95.23, 55, 46, 'test', 'test', 6, 7, '2020-01-01T00:00:00', '2020-01-01T00:00:00', 'test', 'test', 'test', 'test', 'test');
+
+INSERT INTO mmoda_pg_dev.data_product_table_view_v (title, product_path, e1_kev, e2_kev, ra, "dec", time_bin, instrument_name, product_type_name, rev1, rev2, t_start, t_stop, proposal_id, sources, file_name, file_uri, image_uri)
+    VALUES ('test5', 'http://cdciweb02.astro.unige.ch/mmoda/gallery/data-products/emptydummy2022-01-11-121031', 1.0, 2.0, 95.23, 458, 19, 'test', 'test', 6, 7, '2020-01-01T00:00:00', '2020-01-01T00:00:00', 'test', 'test', 'test', 'test', 'test');
--- tests/test_tap.py
+++ tests/test_tap.py
@@ -9,6 +9,18 @@ logger = logging.getLogger(__name__)
 from pytest_postgresql import factories
 from pathlib import Path
 
+postgresql_fixture = factories.postgresql_proc(
+    host="localhost",
+    port=5435,
+    user="postgres",
+    dbname="gallery_dev_prod",
+    password="postgres",
+    load=[Path(os.path.join(os.path.dirname(__file__), 'gallery_pg_db_data/pg_gallery_db_init_dump.sql'))],
+)
+
+postgresql = factories.postgresql(
+    "postgresql_fixture",
+)
 
 @pytest.fixture
 def alter_db_search_path(dispatcher_test_conf_with_vo_options, postgresql):
@@ -17,24 +29,34 @@ def alter_db_search_path(dispatcher_test_conf_with_vo_options, postgresql):
         postgresql.commit()
 
 
-postgresql_fixture = factories.postgresql_proc(
-    host="localhost",
-    port=5435,
-    user="postgres",
-    dbname="gallery_dev_prod",
-    password="postgres",
-    load=[Path(os.path.join(os.path.dirname(__file__), 'gallery_pg_db_data/gallery_dev_dump_empty.sql'))],
-)
-
-postgresql = factories.postgresql(
-    "postgresql_fixture",
-)
+@pytest.fixture
+def fill_up_db(dispatcher_test_conf_with_vo_options, postgresql, alter_db_search_path):
+    with postgresql.cursor() as cur:
+        with open(os.path.join(os.path.dirname(__file__), 'gallery_pg_db_data/pg_gallery_db_init_data_products.sql')) as f:
+            cur.execute(f.read())
+        postgresql.commit()
 
 
 @pytest.mark.test_tap
 def test_local_tap_sync_job_empty_db(dispatcher_live_fixture_with_tap, alter_db_search_path):
     server = dispatcher_live_fixture_with_tap
-    number_results = 0
+    tap_query = f"SELECT * FROM data_product_table_view_v"
+
+    oda_tap = TapPlus(url=os.path.join(server, "tap"))
+
+    ts = oda_tap.launch_job(tap_query)
+
+    print(ts)
+
+    r = ts.get_results()
+
+    assert len(r) == 0
+
+
+@pytest.mark.test_tap
+def test_local_tap_sync_job(dispatcher_live_fixture_with_tap, fill_up_db):
+    server = dispatcher_live_fixture_with_tap
+    number_results = 5
     tap_query = f"SELECT TOP {number_results} * FROM data_product_table_view_v WHERE DISTANCE(POINT(308.107, 40.9577), POINT(ra, dec)) < 107474700"
 
     oda_tap = TapPlus(url=os.path.join(server, "tap"))
