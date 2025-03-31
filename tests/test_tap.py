e2312355ad15968144db7e411ef1b64b54871bf3
burnout87 <gabriele.barni@gmail.com>
2025-03-31 17:39:57 +0200

    testing with pyvo

--- tests/test_tap.py
+++ tests/test_tap.py
@@ -1,6 +1,7 @@
 import os.path
 import logging
 import pytest
+import pyvo
 
 from astroquery.utils.tap.core import TapPlus
 from pytest_postgresql import factories
@@ -44,15 +45,14 @@ def test_local_tap_sync_job_empty_db(dispatcher_live_fixture_with_tap, postgresq
     server = dispatcher_live_fixture_with_tap
     tap_query = f"SELECT * FROM data_product_table_view_v"
 
-    oda_tap = TapPlus(url=os.path.join(server, "tap"))
+    # oda_tap = TapPlus(url=os.path.join(server, "tap"))
+    oda_tap = pyvo.dal.TAPService(os.path.join(server, "tap"))
 
-    ts = oda_tap.launch_job(tap_query)
+    result = oda_tap.search(tap_query)
 
-    print(ts)
+    print(result)
 
-    r = ts.get_results()
-
-    assert len(r) == 0
+    assert len(result) == 0
 
 
 @pytest.mark.test_tap
@@ -61,15 +61,13 @@ def test_local_tap_sync_job(dispatcher_live_fixture_with_tap, fill_up_db):
     number_results = 5
     tap_query = f"SELECT TOP {number_results} * FROM data_product_table_view_v"
 
-    oda_tap = TapPlus(url=os.path.join(server, "tap"))
+    oda_tap = pyvo.dal.TAPService(os.path.join(server, "tap"))
 
-    ts = oda_tap.launch_job(tap_query)
+    result = oda_tap.search(tap_query)
 
-    print(ts)
+    print(result)
 
-    r = ts.get_results()
-
-    assert len(r) == number_results
+    assert len(result) == number_results
 
 
 @pytest.mark.test_tap
@@ -77,14 +75,14 @@ def test_local_tap_load_tables(dispatcher_live_fixture_with_tap, postgresql_fixt
     server = dispatcher_live_fixture_with_tap
     number_results = 1
 
-    oda_tap = TapPlus(url=os.path.join(server, "tap"))
+    oda_tap = pyvo.dal.TAPService(os.path.join(server, "tap"))
 
-    tables = oda_tap.load_tables()
+    tables = oda_tap.tables
 
     print(tables)
 
     assert len(tables) == number_results
 
-    assert tables[0].name == 'data_product_table_view_v'
-    assert tables[0].schema == 'mmoda_pg_dev'
-    assert tables[0].description == 'This is the table of the data_products of the gallery.'
+    tab_names = [tab_name for tab_name in tables.keys()]
+
+    assert tab_names[0] == 'data_product_table_view_v'
